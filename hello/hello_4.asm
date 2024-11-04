081ddf59732fe1f14f05f206859b32b896b49ab6
namickey <teethdai@gmail.com>
2024-11-04 20:46:30 +0900

    add

--- hello/hello_4.asm
+++ hello/hello_4.asm
@@ -1,5 +1,12 @@
 section .text
 _start:
     mov rdx, 0xe
+    mov rsi, msg
+    mov rdi, 0x1
+    mov rax, 0x1
+    syscall
+    mov rdi, 0x0
+    mov rax, 0x3c
+    syscall
 section .rodata
-    msg: db "Hello", 0xa
+    msg: db "Hello, World!", 0xa
